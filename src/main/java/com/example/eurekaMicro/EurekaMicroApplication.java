0a118f7f27dece69fe4266f07178422aade975c7
Meenakshi <Meenakshi@shill>
2023-12-23 11:36:23 +0530

    first commit

---
+++ src/main/java/com/example/eurekaMicro/EurekaMicroApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eurekaMicro/EurekaMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
